a8eae7bb97b27a7dd82bb1e8673c554484c67824
suvarthi <suvarthi.das@phenompeople.com>
2020-04-12 23:09:00 +0530

    commenting the code on each block

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const movieData = [];
 const movies = ["https://www.imdb.com/title/tt7919680/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=f0294795-5981-40c8-b642-15ce8ebdecf6&pf_rd_r=TMWN8SMV9FDE83TBCY2E&pf_rd_s=center-1&pf_rd_t=60601&pf_rd_i=india.toprated&ref_=fea_india_ss_toprated_india_tr_in_cap_pri_1",
     "https://www.imdb.com/title/tt12004706/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=bc7330fc-dcea-4771-9ac8-70734a4f068f&pf_rd_r=FXW24XNY9Y56Y3KSNX8T&pf_rd_s=center-8&pf_rd_t=15021&pf_rd_i=tt7919680&ref_=tt_tp_i_2"];
 
+/* Looping through array of movies and scrapping the data */
+
 for (let movie of movies) {
     (async () => {
         const response = await request({
@@ -20,6 +22,8 @@ for (let movie of movies) {
             }
         });
 
+        /*  Using selector to scrape data from DOM */
+
         const $ = cheerio.load(response);
 
         const title = $(".title_wrapper > h1").text().trim();
@@ -27,12 +31,17 @@ for (let movie of movies) {
         const director = $(".plot_summary > div:nth-child(2) > a").text();
         const summary = $(".plot_summary > .summary_text").text().trim();
 
+        /* Pushing the scrapped  data to CSV file */
+
         movieData.push({
             title,
             rating,
             director,
             summary
         });
+
+
+        /* Creating the CSV file for the response Json */
 
         const json2csvParser = new json2csv();
         const csv = json2csvParser.parse(movieData);
